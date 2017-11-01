6f4b43b6c2de54fc3fee31429608bf556c9b6f10
Deklnwzaza <dietyboy@hotmail.com>
2017-11-02 03:54:54 +0700

    add model

---
+++ Assets/Scripts/moveToImageScreen.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	Application.LoadLevel ("3D Image");
+}
+
+function Update () {
+	
+}
+
+function LoadScene () {
+     Application.LoadLevel ("3D Image");
+ }
